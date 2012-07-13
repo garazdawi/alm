4f344b643612d53de355c0eb0142e7e39386a702
Adam Lindberg <hello@alind.io>
2012-07-13 21:08:55 +0200

    Add help and -S flag to almc

--- compiler/src/almc.erl
+++ compiler/src/almc.erl
@@ -2,16 +2,27 @@
 
 -export([main/1]).
 
-main(Files) ->
-    [compile_file(File) || File <- Files].
+main(Args) ->
+    {ok, {Options, Files}} = getopt:parse(options(), Args),
+    case proplists:get_bool(help, Options) of
+        true -> getopt:usage(options(), atom_to_list(?MODULE));
+        _    -> [compile_file(Options, File) || File <- Files]
+    end.
 
-compile_file(File) ->
+options() ->
+    [{scanner_only, $S, "scanner-only", undefined,
+      "Print scanner output and exit"},
+     {help, $h, "help", undefined,
+      "Print this help and exit"}].
+
+compile_file(Options, File) ->
     {ok, Bin} = file:read_file(File),
-    AST = scan_and_parse(binary_to_list(Bin)),
-    io:format("~p~n", [AST]).
+    Result = scan_and_parse(Options, binary_to_list(Bin)),
+    io:format("~p~n", [Result]).
 
-scan_and_parse(String) ->
+scan_and_parse(Options, String) ->
     Tokens = alm_scanner:scan(String),
-    io:format("DEBUG: ~p~n", [Tokens]),
-    {ok, AST} = alm_parser:parse(Tokens),
-    AST.
+    case proplists:get_bool(scanner_only, Options) of
+        true -> Tokens;
+        _    -> {ok, AST} = alm_parser:parse(Tokens), AST
+    end.
