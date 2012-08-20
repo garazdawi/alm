b950eeb8f44007c6dc0b2c09e8f7cbd8ebbd8f3e
Lukas Larsson <garazdawi@gmail.com>
2012-08-20 23:32:59 +0200

    Remove some debug printouts

--- compiler/src/alm_opt_lift_return.erl
+++ compiler/src/alm_opt_lift_return.erl
@@ -11,16 +11,12 @@ run(OrigCode) ->
     run(lists:reverse(OrigCode),[],[]).
 
 run([{return} = I|T],_,Acc) ->
-    io:format("Found ~p~n",[I]),
     run(T,[I],[I|Acc]);
 run([{move,_,_} = I|T],Is,Acc) when is_list(Is) ->
-    io:format("Found ~p~n",[I]),
     run(T,[I|Is],[I|Acc]);
-run([{label,Lbl} = I|T],Is,Acc) when is_list(Is) ->
-    io:format("Found ~p~n",[I]),
+run([{label,Lbl}|T],Is,Acc) when is_list(Is) ->
     run(T,{Lbl,Is},Acc);
 run([{jump,Lbl}|T],{Lbl,Is},Acc) ->
-    io:format("Found ~p~n",[{jump,Lbl}]),
     run(T,{Lbl,Is},Is++Acc);
 run([I|T],Is,Acc) when is_list(Is) ->
     run(T,undefined,[I|Acc]);
