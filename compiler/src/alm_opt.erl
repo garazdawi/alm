213c618d37774a2933678aab924f53d45d24f81c
Lukas Larsson <garazdawi@gmail.com>
2012-08-22 23:13:59 +0200

    Iterate over optmization passes until code is stable

--- compiler/src/alm_opt.erl
+++ compiler/src/alm_opt.erl
@@ -3,10 +3,23 @@
 -export([run/1,run/2]).
 
 run(Code) ->
-    run(Code,[lift_return,dead_move_elim,dead_move_elim]).
+    run(Code,[{"liveness_analysis",1},"lift_return","dead_move_elim"]).
 
 run(OrigCode, Passes) ->
-    lists:foldl(
-      fun(Pass,Code) ->
-	      (list_to_atom("alm_opt_"++atom_to_list(Pass))):run(Code)
-      end,OrigCode,Passes).
+    lists:foldl(fun run_int/2,OrigCode,Passes).
+
+run_int({Pass,N},Code) when is_list(Pass) ->
+    run_int({list_to_atom("alm_opt_"++Pass),N},Code);
+run_int({_M,0},Code) ->
+    Code;
+run_int({M,N},Code) ->
+    case M:run(Code) of
+	Code ->
+	    Code;
+	NewCode ->
+	    run_int({M,N-1},NewCode)
+    end;
+run_int(Pass,Code) ->
+    run_int({Pass,3},Code).
+
+    
